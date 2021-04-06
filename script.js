272a32c837f9385a3c085ddd055c6a9f23f49cc3
Guzelbaba <valerian.guzelbaba@gmail.com>
2021-04-06 16:34:14 +0200

    2.e

--- script.js
+++ script.js
@@ -5,22 +5,19 @@ document.addEventListener(
         var q1 = false;
         var q2 = false;
         var q3 = false;
-        var q4 = false;
-
+        
         function verifquestionnaire() {
-            if (q1 === true && q2 === true && q3 === true && q4 === true) {
-                b.disabled = false;
+            if (q1 === true && q2 === true && q3 === true) {
+                b.disabled = false;  
             }
         }
-
         var reponsesq1 = document.querySelectorAll('input[name="rep1"]');
         reponsesq1.forEach((e) => {
             e.addEventListener("click", function () {
                 q1 = true;
                 verifquestionnaire();
             });
-        });
-
+        });        
         var reponsesq2 = document.querySelectorAll('input[name="rep2"]');
         reponsesq2.forEach((e) => {
             e.addEventListener("click", function () {
@@ -28,7 +25,6 @@ document.addEventListener(
                 verifquestionnaire();
             });
         });
-
         var reponsesq3 = document.querySelectorAll('input[name="rep3"]');
         reponsesq3.forEach((e) => {
             e.addEventListener("click", function () {
@@ -36,66 +32,46 @@ document.addEventListener(
                 verifquestionnaire();
             });
         });
-
-        var reponsesq4 = document.querySelectorAll('input[name="rep4"]');
-        reponsesq4.forEach((e) => {
-            e.addEventListener("click", function () {
-                q4 = true;
-                verifquestionnaire();
-            });
-        });
-
         b.addEventListener("click", function () {
             b.disabled = true;
             let note = 0;
             let vraireponse1 = 1;
             let vraireponse2 = 2;
             let vraireponse3 = 3;
-            let vraireponse4 = 2;
-
+            let madiv = document.getElementById("maDIV");
+            let madiv4 = document.getElementById("maDIV4");
+            let madiv5 = document.getElementById("maDIV5");
+            let madiv1 = document.getElementById("maDIV1");
+            let madiv2 = document.getElementById("maDIV2");
+            let madiv3 = document.getElementById("maDIV3");
             var valeurreponse1 = document.querySelector('input[name="rep1"]:checked');
             var valeurreponse2 = document.querySelector('input[name="rep2"]:checked');
             var valeurreponse3 = document.querySelector('input[name="rep3"]:checked');
-            var valeurreponse4 = document.querySelector('input[name="rep4"]:checked');
-            console.log("reponse1 : " + valeurreponse1.value);
-            console.log("reponse2 : " + valeurreponse2.value);
-            console.log("reponse3 : " + valeurreponse3.value);
-            console.log("reponse4 : " + valeurreponse4.value);
-
-            if (valeurreponse1.value == vraireponse1) {
-                console.log("la réponse 1 est bonne");
+            
+           if (valeurreponse1.value == vraireponse1) {
                 document.querySelector('input[name="rep1"]:checked').classList.add("juste");
+                madiv1.style.display = "block";
                 note++;
             } else {
-                console.log("la réponse 1 est fausse");
                 document.querySelector('input[name="rep1"]:checked').classList.add("faux");
+                madiv.style.display = "block";
             }
             if (valeurreponse2.value == vraireponse2) {
-                console.log("la réponse 2 est bonne");
                 document.querySelector('input[name="rep2"]:checked').classList.add("juste");
+                madiv2.style.display = "block";
                 note++;
             } else {
-                console.log("la reponse 2 est fausse");
                 document.querySelector('input[name="rep2"]:checked').classList.add("faux");
+                madiv4.style.display = "block";
             }
             if (valeurreponse3.value == vraireponse3) {
-                console.log("la réponse 3 est bonne");
                 document.querySelector('input[name="rep3"]:checked').classList.add("juste");
+                madiv3.style.display = "block";
                 note++;
             } else {
-                console.log("la reponse 3 est fausse");
                 document.querySelector('input[name="rep3"]:checked').classList.add("faux");
-            }
-            if (valeurreponse4.value == vraireponse4) {
-                console.log("la réponse 4 est bonne");
-                document.querySelector('input[name="rep4"]:checked').classList.add("juste");
-                note++;
-            } else {
-                console.log("la reponse 4 est fausse");
-                document.querySelector('input[name="rep4"]:checked').classList.add("faux");
-            }
-
-            console.log("note finale : " + note + "/2");
+                madiv5.style.display = "block";
+            }            
         });
     },
     false
